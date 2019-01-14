ae6067be3220d949a6a6f5c6b6a612970cbc091e
SafarovD <damup1988@gmail.com>
2019-01-15 02:18:35 +0300

    We've created two constructors

--- my_Snake/my_Snake/Point.cs
+++ my_Snake/my_Snake/Point.cs
@@ -13,6 +13,18 @@ namespace my_Snake
         public int y;
         public char sym;
 
+        public Point()
+        {
+            WriteLine("A new point creating");
+        }
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
         public void Draw()
         {
             SetCursorPosition(x, y);
--- my_Snake/my_Snake/Program.cs
+++ my_Snake/my_Snake/Program.cs
@@ -15,16 +15,18 @@ namespace my_Snake
             //char sym1 = '*';
             //Draw(x1, y1, sym1);
 
-            Point point1 = new Point { x = 1, y = 3, sym = '*'};
+            //Point point1 = new Point { x = 1, y = 3, sym = '*'};
             //Draw(point1.x, point1.y, point1.sym);
+            Point point1 = new Point(1,3,'*');
             point1.Draw();
 
             //int x2 = 3, y2 = 5;
             //char sym2 = '#';
             //Draw(x2, y2, sym2);
 
-            Point point2 = new Point { x = 3, y = 5, sym = '#' };
+            //Point point2 = new Point { x = 3, y = 5, sym = '#' };
             //Draw(point2.x, point2.y, point2.sym);
+            Point point2 = new Point(3, 5, '#');
             point2.Draw();
 
             ReadLine();

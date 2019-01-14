7579b07aa746fda990b80bb02726aaf1857c12e9
SafarovD <damup1988@gmail.com>
2019-01-15 01:14:36 +0300

    function Draw()

--- my_Snake/my_Snake/Program.cs
+++ my_Snake/my_Snake/Program.cs
@@ -11,8 +11,22 @@ namespace my_Snake
     {
         static void Main(string[] args)
         {
-            WriteLine("Hellow world!");
+            int x1 = 1, y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+
+            int x2 = 3, y2 = 5;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+
+
             ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            SetCursorPosition(x, y);
+            WriteLine(sym);
         }
     }
 }

5fea3a6e7a9841cfdee507b8ed07ebce000cfc68
SafarovD <damup1988@gmail.com>
2019-01-15 01:51:34 +0300

    a class has been added (Point)

---
+++ my_Snake/my_Snake/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace my_Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()
+        {
+            SetCursorPosition(x, y);
+            WriteLine(sym);
+        }
+    }
+}
--- my_Snake/my_Snake/Program.cs
+++ my_Snake/my_Snake/Program.cs
@@ -11,22 +11,29 @@ namespace my_Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1, y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
+            //int x1 = 1, y1 = 3;
+            //char sym1 = '*';
+            //Draw(x1, y1, sym1);
 
-            int x2 = 3, y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
+            Point point1 = new Point { x = 1, y = 3, sym = '*'};
+            //Draw(point1.x, point1.y, point1.sym);
+            point1.Draw();
 
+            //int x2 = 3, y2 = 5;
+            //char sym2 = '#';
+            //Draw(x2, y2, sym2);
+
+            Point point2 = new Point { x = 3, y = 5, sym = '#' };
+            //Draw(point2.x, point2.y, point2.sym);
+            point2.Draw();
 
             ReadLine();
         }
 
-        static void Draw(int x, int y, char sym)
-        {
-            SetCursorPosition(x, y);
-            WriteLine(sym);
-        }
+        //static void Draw(int x, int y, char sym)
+        //{
+        //    SetCursorPosition(x, y);
+        //    WriteLine(sym);
+        //}
     }
 }

de1d8b1eade2adabd57ca7e0831ad17f16489993
SafarovD <damup1988@gmail.com>
2019-01-15 01:06:00 +0300

    hello world

---
+++ my_Snake/my_Snake/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace my_Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hellow world!");
+            ReadLine();
+        }
+    }
+}
